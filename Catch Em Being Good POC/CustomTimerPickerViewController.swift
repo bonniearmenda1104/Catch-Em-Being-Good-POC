3f76d21ebd841080af25af091a4d471f6434130b
Bonnie Chan <bonniechan@Bonnies-MacBook-Pro.local>
2016-11-19 19:59:45 -0800

    Adding custom picker view controller

---
+++ Catch Em Being Good POC/CustomTimerPickerViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CustomTimerPickerViewController.swift
+//  Catch Em Being Good POC
+//
+//  Created by Bonnie Chan on 11/19/16.
+//  Copyright © 2016 TsangChan Inc. All rights reserved.
+//
+
+import UIKit
+
+class CustomTimerPickerViewController: UIViewController {
+
+    @IBOutlet weak var TimePicker: UIPickerView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
