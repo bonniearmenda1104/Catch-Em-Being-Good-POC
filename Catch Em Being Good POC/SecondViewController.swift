d7fab2d7a006a00621e3ff43fca7dcf0a51921b2
Bonnie Chan <bonniechan@Bonnies-MacBook-Pro.local>
2016-11-19 19:27:35 -0800

    Test commit 1

--- Catch Em Being Good POC/SecondViewController.swift
+++ Catch Em Being Good POC/SecondViewController.swift
@@ -15,6 +15,8 @@ class SecondViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var TimerWheel: UIPickerView!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
